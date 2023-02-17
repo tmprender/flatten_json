dbd0bdaeef4aa19266346109ddcf3e1e9cd224c8
Thomas Prenderville <tom@TMP-MBP.local>
2023-02-16 21:18:34 -0500

    working with lists of depth 1, sloppy

--- flatten.py
+++ flatten.py
@@ -5,41 +5,58 @@ GLOBAL_STRING = ""
 
 def parse_json_to_string(json_data, path):
     global GLOBAL_STRING
-    # base case
+
+    # base cases
+
+    # if a list is passed directly to method, then return
+    # parser has depth of 1 with nested lists
+    if type(json_data) is list:
+        GLOBAL_STRING += path + "=" + str(json_data) + "\n"
+        return GLOBAL_STRING
+    
+    # no (more) keys to parse
     if len(json_data.keys()) < 1:
         return GLOBAL_STRING
 
-    # full path to key-value pair
-    parent = ""
+    # store path for current run before updating
+    parent = path
     
     for key in json_data.keys():
         # check for nested json block
         if type(json_data[key]) is dict:
-            # store path to this nested block as its parent
-            parent = path
             # build path.to.key=values as string
             path += key + "."
-
-            ### for debugging.. ###
-            #print("TMP PATH: ", path)
-            #print("SENDING -> ", json_data[key])
-            ### --------------- ###
-
             # recurse until value is non-dict
             parse_json_to_string(json_data[key], path)
             # reset path for this loop now that method has returned
             path = parent
 
+        # check for list --- WIP --- ne
+        elif type(json_data[key]) is list:
+            # iterate thru elements
+            for i in json_data[key]:
+                # check if nested as dict
+                if type(i) is dict:
+                    # update path indicating list element
+                    path +=  key + "[]."      
+                    parse_json_to_string(i, path)
+                    path = parent
+                # nested list?
+                elif type(i) is list:
+                    path += key + "[]"
+                    parse_json_to_string(i, path)
+                    path = parent
+                else:
+                    GLOBAL_STRING += path + key + "[]="
+                    GLOBAL_STRING += str(i) + "\n"
+
+
         # not nested, write path.to.key=value pair(s) for this block
         else:
-            #print("ADDING TO STRING ::: ", path, key, json_data[key], "\n")
             GLOBAL_STRING += path + key + "="
             GLOBAL_STRING += str(json_data[key]) + "\n"
 
     # return to caller
-    #print("parent: ", parent, "path: ", path)
-    print("RESULT:")
-    print(GLOBAL_STRING)
     return GLOBAL_STRING
 
 if __name__ == "__main__":
@@ -47,4 +64,5 @@ if __name__ == "__main__":
         s = f.read()
 
     j = json.loads(s)  # oh the irony
-    parse_json_to_string(j, "")
+    result = parse_json_to_string(j, "")
+    print(result)
